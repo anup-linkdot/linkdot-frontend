56e2ace35364cd60f563ec494368dae7520b4ec7
sreejinsreenivasan <sreejinsreenivasan93@gmail.com>
2022-06-29 15:40:19 +0530

    Add new sidebar component

--- src/components/Dashboard/Main.js
+++ src/components/Dashboard/Main.js
@@ -1,26 +1,14 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import "../../styles/dashboard.styles.scss";
-import MenuItem from "./MenuItem";
+import SideBar from "./SideBar";
 
 const Main = () => {
-  const navLinks = [
-    { name: "Dashboard", url: "/dashboard/nobadge", path: "/dashboard/" },
-    { name: "Badge Insight", url: "/insights", path: "/insights/" },
-  ];
-  const currentPath = window.location.pathname;
   return (
     <div className="main-dashboard-div">
       <div className="main-row">
         <div className="button-left-main">
-          {navLinks.map(({ name, url, path }) => (
-            <MenuItem
-              key={name}
-              name={name}
-              active={currentPath === { path }}
-              url={url}
-            />
-          ))}
+          <SideBar />
         </div>
         <div className="main-section-div">
           <Outlet />
---
+++ src/components/Dashboard/SideBar.js
@@ -0,0 +1,24 @@
+import MenuItem from "./MenuItem";
+
+const SideBar = () => {
+  const navLinks = [
+    { name: "Dashboard", url: "/dashboard/nobadge", path: "/dashboard/" },
+    { name: "Badge Insight", url: "/insights", path: "/insights" },
+  ];
+  const currentPath = window.location.pathname;
+
+  return (
+    <>
+      {navLinks.map(({ name, url, path }) => (
+        <MenuItem
+          key={name}
+          name={name}
+          active={currentPath === path}
+          url={url}
+        />
+      ))}
+    </>
+  );
+};
+
+export default SideBar;
